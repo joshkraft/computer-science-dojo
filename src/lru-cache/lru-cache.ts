6b09a58efa6db528a260f5d20a94c7808a2a2f2e
Josh Kraft <joshkraft757@gmail.com>
2022-08-28 18:02:37 -0600

    implement lru cache

--- src/lru-cache/lru-cache.ts
+++ src/lru-cache/lru-cache.ts
@@ -1,3 +1,82 @@
 export class LRUCache {
-  // TODO
+  data: Map<number, DoublyLinkedListNode>;
+  capacity: number;
+  head: DoublyLinkedListNode;
+  tail: DoublyLinkedListNode;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.data = new Map();
+    this.head = new DoublyLinkedListNode(-1, -1);
+    this.tail = new DoublyLinkedListNode(-1, -1);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  get(key: number): number {
+    const existingNode: DoublyLinkedListNode | undefined = this.data.get(key);
+
+    if (existingNode) {
+      this.removeNode(existingNode);
+      this.insertMRUNode(existingNode);
+      return existingNode.val;
+    }
+
+    return -1;
+  }
+
+  put(key: number, value: number): void {
+    const existingNode: DoublyLinkedListNode | undefined = this.data.get(key);
+
+    if (existingNode) {
+      this.removeNode(existingNode);
+    }
+
+    const newNode: DoublyLinkedListNode = new DoublyLinkedListNode(key, value);
+    this.insertMRUNode(newNode);
+    this.data.set(key, newNode);
+
+    const isOverCapacity = this.data.size > this.capacity;
+
+    if (isOverCapacity) {
+      const lruNode: DoublyLinkedListNode | null = this.head.next;
+
+      if (lruNode) {
+        this.removeNode(lruNode);
+        this.data.delete(lruNode.key);
+      }
+    }
+  }
+
+  removeNode(node: DoublyLinkedListNode): void {
+    const prev = node!.prev;
+    const nxt = node!.next;
+
+    [prev!.next, nxt!.prev] = [nxt, prev];
+  }
+
+  insertMRUNode(node: DoublyLinkedListNode): void {
+    const prevMRUNode = this.tail.prev;
+    const tailNode = this.tail;
+
+    prevMRUNode!.next = node;
+    tailNode.prev = node;
+
+    node.next = tailNode;
+    node.prev = prevMRUNode;
+  }
+}
+
+export class DoublyLinkedListNode {
+  key: number;
+  val: number;
+  next: DoublyLinkedListNode | null;
+  prev: DoublyLinkedListNode | null;
+
+  constructor(key: number, val: number) {
+    this.key = key;
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
