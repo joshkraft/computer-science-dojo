ce105517f90176ea5bedc7920569815297575aa6
Josh Kraft <joshkraft757@gmail.com>
2022-11-05 08:36:59 -0600

    poc implementation of 'practice' script

---
+++ src/utils/delete-method-bodies.ts
@@ -0,0 +1,52 @@
+import {readFileSync, writeFileSync} from 'fs';
+import ts = require('typescript');
+
+const filePath = process.argv[2];
+
+let className = filePath
+  .split('/')
+  [filePath.split('/').length - 1].slice(0, -3);
+className = className.charAt(0).toUpperCase() + className.slice(1);
+
+const rawFile = readFileSync(filePath, 'utf-8');
+
+const sourceFile: ts.SourceFile = ts.createSourceFile(
+  filePath,
+  rawFile,
+  ts.ScriptTarget.Latest
+);
+
+const linesToDrop = new Set();
+
+ts.forEachChild(sourceFile, node => {
+  if (ts.isClassDeclaration(node) && node.name?.text === className) {
+    const methods = node.members.filter(member =>
+      ts.isMethodDeclaration(member)
+    );
+
+    for (const method of methods) {
+      const startLine = sourceFile.getLineAndCharacterOfPosition(
+        method.getStart(sourceFile)
+      ).line;
+
+      const stopLine = sourceFile.getLineAndCharacterOfPosition(
+        method.getEnd()
+      ).line;
+
+      for (let i = startLine + 1; i < stopLine; i++) {
+        linesToDrop.add(i);
+      }
+    }
+  }
+});
+
+const allLines = rawFile.split('\n');
+const selectedLines = [];
+
+for (let i = 0; i < allLines.length; i++) {
+  if (!linesToDrop.has(i)) {
+    selectedLines.push(allLines[i]);
+  }
+}
+
+writeFileSync(filePath, selectedLines.join('\n'));
