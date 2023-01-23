9f12a9b573b7ce38079a67959248b0a2a846c804
Josh Kraft <joshkraft757@gmail.com>
2023-01-23 06:47:08 -0700

    dfs in binary tree

--- src/dfs-binary-tree.ts
+++ src/dfs-binary-tree.ts
@@ -15,36 +15,38 @@ export class Node {
 
 /**
  * Searches a binary tree for a node that matches the provided target, using the
- * breadth first search algorithm.
+ * depth first search algorithm.
  *
- * The breadth first search algorithm works by utilizing a queue to visit all
- * nodes at a 'level' in the tree before moving on to the next level.
- * The algorithm continues searching farther down the levels of the tree until
+ * The depth first search algorithm works by utilizing a stack (call stack or
+ * stack data structure) to visit all nodes in a 'depth first' manner. The
+ * algorithm explores as far as possible along each branch of the tree before
+ * backtracking and exploring the next branch of the tree. This continues until
  * the target is found, or all nodes have been visited.
  *
  * @param root The root node of the binary tree.
  * @param target The number to search for in the binary tree.
  * @returns A boolean indicating if the target value exists in the binary tree.
- * @space O(n)
+ * @space O(h)
  * @time O(n)
+ * - h: height of the tree
  * - n: number of nodes in the tree
  */
-export function bfsBinaryTree(root: Node, target: number): boolean {
-  const queue: Node[] = [root];
+export function dfsBinaryTree(root: Node, target: number): boolean {
+  const stack: Node[] = [root];
 
-  while (queue.length) {
-    const curr: Node = queue.shift() as Node;
+  while (stack.length) {
+    const curr: Node = stack.pop() as Node;
 
     if (curr.val === target) {
       return true;
     }
 
     if (curr.left) {
-      queue.push(curr.left);
+      stack.push(curr.left);
     }
 
     if (curr.right) {
-      queue.push(curr.right);
+      stack.push(curr.right);
     }
   }
 
